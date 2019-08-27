147ca0e025dbd1e4bd61d0c1f316d5dd9ddef8e8
github username <github email address>
2019-08-27 16:02:30 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,23 @@
+require "pry"
+
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition, :brand
+  attr_reader :brand 
 
+  BRANDS = []
+# binding.pry
   def initialize(brand)
-    @brand = brand
+    @brand = brand 
+    if !BRANDS.include?(brand)
+      BRANDS << brand
+    end 
   end
-
+  # binding.pry 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
 
+  # binding.pry
 end
+#binding.pry 
